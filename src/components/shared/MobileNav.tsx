fd91a5f622f659a75805943665ba96a6d36254e6
Rakshith <rakkshith2001@gmail.com>
2024-05-28 21:44:43 +0530

    first commit

--- src/components/shared/MobileNav.tsx
+++ src/components/shared/MobileNav.tsx
@@ -15,7 +15,7 @@ const MobileNav = () => {
     <header className="header">
       <Link href="/" className="flex items-center gap-2 md:py-2">
         <Image
-          src="/assets/images/logo-text.svg"
+          src="/assets/images/logo.png"
           alt="logo"
           width={180}
           height={28}
