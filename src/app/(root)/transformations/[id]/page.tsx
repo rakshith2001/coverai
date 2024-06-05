de2332e632250244a625b279db904491d2957aa3
Rakshith <rakkshith2001@gmail.com>
2024-06-05 14:12:36 +0530

    bug fixes and added cover letter download

--- src/app/(root)/transformations/[id]/page.tsx
+++ src/app/(root)/transformations/[id]/page.tsx
@@ -1,17 +1,24 @@
 "use client"
-import React, { use } from 'react'
-import { any } from 'zod'
-import Header from '@/components/shared/Header'
-import { getProfileByParams, updateProfileByParams } from '@/lib/actions/profile.actions'
 
-import { zodResolver } from "@hookform/resolvers/zod"
+import React, { useState, useEffect } from 'react'
 import { useForm } from "react-hook-form"
 import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
 import { useRouter } from "next/router"
-import { useState, useEffect } from "react"
-import Link from 'next/link'
-import { useToast } from '@/components/ui/use-toast'; 
-
+import Header from '@/components/shared/Header'
+import { getProfileByParams, updateProfileByParams } from '@/lib/actions/profile.actions'
+import { useToast } from '@/components/ui/use-toast'
+import { Input } from "@/components/ui/input"
+import { Textarea } from '@/components/ui/textarea'
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { CustomField } from "@/components/shared/CustomField"
 import {
     Select,
     SelectContent,
@@ -19,23 +26,14 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select"
-
-import { CustomField } from "@/components/shared/CustomField"
-import { Button } from "@/components/ui/button"
-import { Textarea } from '@/components/ui/textarea'
-import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from "@/components/ui/form"
 import { aspectRatioOptions, creditFee, defaultValues, transformationTypes } from "@/constants"
+import { Button } from "@/components/ui/button"
 
-import { Input } from "@/components/ui/input"
-import { Description } from "@radix-ui/react-dialog"
+interface SearchParamProps {
+    params: {
+        id: string;
+    };
+}
 
 const formSchema = z.object({
     name: z.string().nonempty("Name is required"),
@@ -44,102 +42,110 @@ const formSchema = z.object({
 })
 
 const UpdateProfile = ({ params: { id } }: SearchParamProps) => {
-  const { toast } = useToast()
-  const [profile, setProfile] = useState({ name: '', workExperience: '', description: '' })
+    const { toast } = useToast()
+    const [profile, setProfile] = useState(null)
+    const form = useForm({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: '',
+            workExperience: '',
+            description: ''
+        },
+    })
 
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: profile,
-  })
+    useEffect(() => {
+        const findProfile = async () => {
+            try {
+                const profileData = await getProfileByParams(id)
+                setProfile(profileData)
+                form.reset(profileData)
+            } catch (error) {
+                console.error("Error fetching profile:", error)
+            }
+        }
+        findProfile()
+    }, [id, form])
 
-  const findProfile = async () => {
-    const profileData = await getProfileByParams(id)
-    setProfile(profileData)
-    form.reset(profileData)
-  }
-
-  useEffect(() => {
-    findProfile()
-  }, [])
-
-  async function onSubmit(values: z.infer<typeof formSchema>) {
-    const updatedProfile = await updateProfileByParams(id, values)
-    // handle the response or redirection after successful update
-    if (updatedProfile) {
-      toast({
-        title: "Profile Updated",
-        description: "Your profile has been successfully updated.",
-        duration: 5000,
-        className: 'success-toast'
-      });
-      // handle the response or redirection after successful update
+    async function onSubmit(values: z.infer<typeof formSchema>) {
+        try {
+            const updatedProfile = await updateProfileByParams(id, values)
+            if (updatedProfile) {
+                toast({
+                    title: "Profile Updated",
+                    description: "Your profile has been successfully updated.",
+                    duration: 5000,
+                    className: 'success-toast'
+                })
+            } else {
+                toast({
+                    title: "Profile Update Failed",
+                    description: "Your profile could not be updated.",
+                    duration: 5000,
+                    className: 'error-toast'
+                })
+            }
+        } catch (error) {
+            console.error("Error updating profile:", error)
+            toast({
+                title: "Profile Update Failed",
+                description: "Your profile could not be updated.",
+                duration: 5000,
+                className: 'error-toast'
+            })
+        }
     }
-    else{
-      toast({
-        title: "Profile Update Failed",
-        description: "Your profile could not be updated.",
-        duration: 5000,
-        className: 'error-toast'
-      
-      })
-    }
-    
-  }
 
-  return (
-    <>
-      <Header
-        title='Update Profile'
-        subtitle='Your profile information'
-      />
-      <section className="mt-10">
-        <Form {...form}>
-          <form className="space-y-8" onSubmit={form.handleSubmit(onSubmit)}>
-            <div className="flex flex-wrap -mx-2">
-              <div className="w-1/2 px-2">
-                <CustomField
-                  control={form.control}
-                  name="name"
-                  formLabel="Name"
-                  className="w-full"
-                  render={({ field }) => <Input {...field} className="input-field" />}
-                />
-              </div>
-              <div className="w-1/2 px-2">
-                <CustomField
-                  control={form.control}
-                  name="workExperience"
-                  formLabel="Work Experience"
-                  className="w-full"
-                  render={({ field }) => <Input {...field} className="input-field" />}
-                />
-              </div>
-              <div className="w-full px-2">
-                <FormField
-                  control={form.control}
-                  name="description"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel></FormLabel>
-                      <FormControl>
-                        <Textarea
-                          placeholder="Tell us a little bit about yourself"
-                          className="resize-none"
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-            </div>
-            <button type="submit" className="submit-button capitalize">Submit</button>
-          </form>
-        </Form>
-      </section>
-    </>
-  )
+    return (
+        <>
+            <Header title='Update Profile' subtitle='Your profile information' />
+            <section className="mt-10">
+                <Form {...form}>
+                    <form className="space-y-8" onSubmit={form.handleSubmit(onSubmit)}>
+                        <div className="flex flex-wrap -mx-2">
+                            <div className="w-1/2 px-2">
+                                <CustomField
+                                    control={form.control}
+                                    name="name"
+                                    formLabel="Name"
+                                    className="w-full"
+                                    render={({ field }) => <Input {...field} className="input-field" />}
+                                />
+                            </div>
+                            <div className="w-1/2 px-2">
+                                <CustomField
+                                    control={form.control}
+                                    name="workExperience"
+                                    formLabel="Work Experience"
+                                    className="w-full"
+                                    render={({ field }) => <Input {...field} className="input-field" />}
+                                />
+                            </div>
+                            <div className="w-full px-2">
+                                <FormField
+                                    control={form.control}
+                                    name="description"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Description</FormLabel>
+                                            <FormControl>
+                                                <Textarea
+                                                    placeholder="Tell us a little bit about yourself"
+                                                    className="resize-none"
+                                                    {...field}
+                                                />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                            </div>
+                        </div>
+                        <Button type="submit" className="submit-button capitalize">Submit</Button>
+                    </form>
+                </Form>
+            </section>
+        </>
+    )
 }
 
 export default UpdateProfile
